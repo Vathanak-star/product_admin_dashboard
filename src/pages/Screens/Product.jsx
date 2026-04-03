57f340aa4bceb258dad8ea25862e77fb2485330c
Vathanak-star <khunvathanak@gmail.com>
2026-04-03 23:38:02 +0700

    product

--- src/pages/Screens/Product.jsx
+++ src/pages/Screens/Product.jsx
@@ -10,14 +10,102 @@ import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, TextF
 
 const desc = "AMD Ryzen™ 9 9955HX NVIDIA® GeForce RTX™ 5070 16GB DDR5 1TB PCIe® 4.0 NVMe™ M.2 SSD FHD+ 165Hz Windows 11 Home"
 
-const rows = [
-    {id: 1, name: 'Asus ROG STRIX G19', image: 'Base64_Img',price: 1999.99,description: desc,category: 'Laptop',status: 'active'},
-    {id: 2, name: 'Laptop Asus ROG STRIX', image: 'Base64_Img',price: 1999.99,description: desc,category: 'Laptop',status: 'active'},
-    {id: 3, name: 'Laptop Asus ROG STRIX', image: 'Base64_Img',price: 1999.99,description: desc,category: 'Laptop',status: 'active'},
-    {id: 4, name: 'Laptop Asus ROG STRIX', image: 'Base64_Img',price: 1999.99,description: desc,category: 'Laptop',status: 'active'},
-]
+export default function Product(){
+    const [items, setItems] =useState([])
+    const [openAdd,setOpenAdd] = useState(false)
+    const [categories,setCategories] = useState([])
+    const [selectValue,setSelectValue] = useState('')
+    const [statusValue,setStatusValue] = useState('')
+    const [catValue,setCatValue] = useState('')
 
-const columns = [
+    const [name,setName] = useState('')
+    const [description,setDescription] = useState('')
+    const [price,setPrice] = useState()
+    const [image,setImage] = useState('')
+    const [openUp,setOpenUp] = useState(false)
+    const [updateId,setUpdateId] = useState(0)
+
+
+    const [search,setSearch] = useState('')
+    const [filterCat,setFilterCat] = useState('')
+
+    const resetField = () => {
+        setName('')
+        setPrice()
+        setDescription('')
+        setImage('')
+        setSelectValue('')
+        setStatusValue('')
+    }
+
+
+    const handleOpenAdd = () => setOpenAdd(true)
+    const handleCloseAdd = () => {
+      setOpenAdd(false)
+      resetField()
+    }
+
+    const handleOpenUP = () => setOpenUp(true)
+    const handleCloseUp = () => {
+      setOpenUp(false)
+      resetField()
+    }
+
+    const handleSearchItem = () => {
+      const tempItems = [...items]
+      const searchItem = tempItems.filter((item) => item.name.toLowerCase().startsWith(search))
+      console.log(searchItem)
+      setItems(searchItem)
+      
+    }
+
+
+    const handleDeleteItem = async (id) => {
+      try {
+        const result = await itemsService.deleteItem(id)
+        console.log(result)
+        setItems(item => item.filter(i => i.id !== id))
+        console.log('Delete Item finish')
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    const handleUpdateItem = async () => {
+      const ItemObj = {
+        name: name,
+        price: price,
+        description: description,
+        image: image,
+        category: selectValue,
+        status: statusValue
+      }
+      try {
+        const result = await itemsService.updateItem(ItemObj,updateId)
+        console.log(result)
+        const item = items.find(item => item.id == updateId)
+        const newItem = {item,
+          id: updateId,
+          name: name,
+          price: price,
+          description: description,
+          image: image,
+          category: selectValue,
+          status: statusValue
+        }
+        const itemIndex = items.findIndex(item => item.id == updateId)
+        const newData = [...items]
+        newData[itemIndex] = newItem
+        setItems(newData)
+        setOpenUp(false)
+        resetField()
+
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    const columns = [
       { field: 'id', headerName: 'ID', flex:1},
       {
         field: 'name',
@@ -54,7 +142,14 @@ const columns = [
         headerName: 'Update',
         renderCell: (params) => (
           <Button sx={{width: 60, height: 30}} variant="contained" onClick={() => {
-   
+            setUpdateId(params.id)
+            setName(params.row.name)
+            setPrice(params.row.price)
+            setDescription(params.row.description)
+            setImage(params.row.image)
+            setSelectValue(params.row.category)
+            setStatusValue(params.row.status)
+            setOpenUp(true)
           }}>
             <RefreshCcw className="w-5 h-5"/>
           </Button>
@@ -65,32 +160,16 @@ const columns = [
         field: 'delete',
         headerName: 'Delete',
         renderCell: (params) => (
-          <Button sx={{width: 60, height: 30}} color="error" variant="contained" onClick={() => {
-            console.log('Row ID: ',params.row.image)
-          }}>
+          <Button sx={{width: 60, height: 30}} color="error" variant="contained" onClick={ () => {
+            handleDeleteItem(params.id)
+          }
+          }>
             <Trash/>
           </Button>
         ),
         width: 90
       },
     ];
-
-export default function Product(){
-    const [items, setItems] =useState([])
-    const [openAdd,setOpenAdd] = useState(false)
-    const [categories,setCategories] = useState([])
-    const [selectValue,setSelectValue] = useState('')
-    const [statusValue,setStatusValue] = useState('')
-    const [catValue,setCatValue] = useState('')
-
-    const [name,setName] = useState('')
-    const [description,setDescription] = useState('')
-    const [price,setPrice] = useState('')
-    const [image,setImage] = useState('')
-
-
-    const handleOpenAdd = () => setOpenAdd(true)
-    const handleCloseAdd = () => setOpenAdd(false)
 
     useEffect(() => {
       categoryService.getSubCategories().then(subCategories => {
@@ -122,6 +201,7 @@ export default function Product(){
         setItems([...items,item.data])
 
         setOpenAdd(false)
+        resetField()
       }).catch(error => {
         console.log(error.message)
       })
@@ -137,25 +217,26 @@ export default function Product(){
             <div className="p-6">
                 <div className="flex w-full items-center justify-between">
                     <div className="flex">
-                        <div class="relative flex items-center w-60 lg:w-80 transition-all">
+                        <div className="relative flex items-center w-60 lg:w-80 transition-all">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600">
-                            <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
+                            <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
                             </svg>
                         
                             <input
-                                className="w-full bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-l-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                                value={search} onChange={(e) => setSearch(e.target.value)} className="w-full bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-l-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
                                 placeholder="Search Products..." 
                             />
                             
                             <button
                             className="rounded-r-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-0"
                             type="button"
+                            onClick={handleSearchItem}
                             >
                             Search
                             </button> 
                         </div>
 
-                        <div class="relative w-45 lg:w-70 transition-all ml-3">
+                        <div className="relative w-45 lg:w-70 transition-all ml-3">
                         <select
                                 value={catValue} onChange={e => setCatValue(e.target.value)} class="w-full transition-all bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
                                 <option selected>Filter By Category</option>
@@ -164,7 +245,7 @@ export default function Product(){
                                 })}
                             </select>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                             </svg>
                         </div>
                     </div>
@@ -196,8 +277,44 @@ export default function Product(){
             <Dialog open={openAdd} fullWidth>
                           <DialogTitle>Add New Product</DialogTitle>
                           <DialogContent>
-                            <TextField value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" ></TextField>
-                            <TextField value={price} onChange={(e) => setPrice(Number(e.target.value))} placeholder="Price" ></TextField>
+                            <TextField fullWidth value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" ></TextField>
+                            <form className="w-full mx-auto my-3">
+                              <input value={price} onChange={(e) => setPrice(e.target.value)} type="number" id="number-input" aria-describedby="helper-text-explanation" className="block w-full px-3 py-3 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-lg focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" placeholder="Price $: 99.99" required />
+                            </form>
+                            <Stack spacing={2}>
+                              <TextField value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" ></TextField>
+                              <TextField value={image} onChange={(e) => setImage(e.target.value)} placeholder="Image" ></TextField>
+                            </Stack>
+            
+                            <select value={selectValue} id="mainCategory" onChange={e => setSelectValue(e.target.value)} className="block mt-3 w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-lg focus:ring-brand focus:border-brand shadow-xs placeholder:text-body">
+                                        <option selected>Choose a category</option>
+                                    
+                                        {categories.map(category => 
+                                          <option key={category.id} value={category.name}>{category.name}</option>
+                                        )}
+                            </select>
+
+                            <select value={statusValue} id="status" onChange={e => setStatusValue(e.target.value)} className="block mt-3 w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-lg focus:ring-brand focus:border-brand shadow-xs placeholder:text-body">
+                                        <option selected>Select status</option>
+                                        <option value="active">active</option>
+                                        <option value="out of stock">out of stock</option>
+                            </select>
+                          </DialogContent>
+            
+                          <DialogActions>
+                              <Button onClick={onAddNewItem} color="success">Submit</Button>
+                              <Button onClick={handleCloseAdd} color="error">Cancel</Button>
+                          </DialogActions>
+            </Dialog>
+
+
+            <Dialog open={openUp} fullWidth>
+                          <DialogTitle>Update Product</DialogTitle>
+                          <DialogContent>
+                            <TextField fullWidth value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" ></TextField>
+                            <form className="w-full mx-auto my-3">
+                              <input value={price} onChange={(e) => setPrice(e.target.value)} type="number" id="number-input" aria-describedby="helper-text-explanation" className="block w-full px-3 py-3 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-lg focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" placeholder="Price $: 99.99" required />
+                          </form>
                             <Stack spacing={2}>
                               <TextField value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" ></TextField>
                               <TextField value={image} onChange={(e) => setImage(e.target.value)} placeholder="Image" ></TextField>
@@ -220,8 +337,8 @@ export default function Product(){
                           </DialogContent>
             
                           <DialogActions>
-                              <Button onClick={onAddNewItem} color="success">Submit</Button>
-                              <Button onClick={handleCloseAdd} color="error">Cancel</Button>
+                              <Button onClick={handleUpdateItem} color="success">Submit</Button>
+                              <Button onClick={handleCloseUp} color="error">Cancel</Button>
                           </DialogActions>
             </Dialog>
         </div>
--- src/services/items.js
+++ src/services/items.js
@@ -11,4 +11,16 @@ const createItem = async (itemObj) => {
      return response.data
 }
 
-export default {getAllItem,createItem}
+const updateItem = async (itemObj,id) => {
+     const response = await axios.post(`${baseUrl}/updateItem/${id}`,itemObj)
+     return response.data
+}
+
+const deleteItem = async (id) => {
+     const response = await axios.delete(`${baseUrl}/deleteItem/${id}`)
+     return response.data
+}
+
+
+
+export default {getAllItem,createItem,updateItem,deleteItem}
